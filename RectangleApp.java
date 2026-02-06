633aee75b8e3ea280a3d6ec7e988a8d562fe771e
oh413 <oh413@exeter.ac.uk>
2026-02-06 10:15:18 +0000

    completed part 4

--- RectangleApp.java
+++ RectangleApp.java
@@ -50,7 +50,7 @@ public class RectangleApp {
 		boolean result = Rectangle.areOverlapping(myRect4, myRect5);
 		System.out.println( "myRect4 overlaps myRect5: " + result);
 
-		System.out.println("Check class prevents negative widths");
+		System.out.println("\nCheck class prevents negative widths");
 
 		// initialise rectangle for test
 		Rectangle myRect7 = new Rectangle(30.0, 5.0, 10, 10); 
@@ -63,5 +63,19 @@ public class RectangleApp {
 		// change to negative width should be ignored
 		myRect7.setWidth(-10);
 		System.out.println( "Width: "+myRect7.getWidth()+", Height: "+myRect7.getHeight() );
+
+
+		Rectangle r1 = new Rectangle(10.0,5.0);
+		Rectangle r2 = new Rectangle(10.0,5.0);
+		Rectangle r3 = r2;
+
+		System.out.println("\nObject reference tests:");
+		System.out.println("r1: " + r1);
+		System.out.println("r2: " + r2);
+		System.out.println("r3: " + r3);
+
+		r2.scale(0.5);
+		System.out.println("r2 width: " + r2.getWidth());
+		System.out.println("r3 width: " + r3.getWidth());
 	}
 }
